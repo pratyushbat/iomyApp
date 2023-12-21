6df735dddd383c6ea620253d37813408ff208fab
pratyush kumar <pratyush845@gmail.com>
2023-12-21 08:24:48 +0530

    added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  menuType: string = 'overlay';
 
+  items: string[] = [];
+
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
   constructor() {}
+  ngOnInit(): void {
+    this.generateItems();
+  }
 
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev: any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+  isModalOpen = false;
+
+  setOpenMd(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 }
